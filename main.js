9a1273ec1543503d27e016a56640dbab3e2a1371
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-07-06 12:33:31 -0300

    challenge done

---
+++ main.js
@@ -0,0 +1,47 @@
+// 1
+/* alert('Hello World') */
+
+// 2
+/* let var1 = 1
+let var2 = 2
+alert(`O resultado da soma é: ${var1 + var2}`) */
+
+// 3
+/* let m = 2
+typeof m == 'number' ? alert('É um número') : alert('Não é um número') */
+
+// 4
+/* let m = 0
+typeof m == 'string' ? alert('É uma string') : alert('Não é uma string') */
+
+// 5
+/* let m = 0
+typeof m == 'boolean' ? alert('É um booleano') : alert('Não é um booleano') */
+
+// 6
+/* let n1 = 1,
+  n2 = 0.5
+
+alert(`O resultado da subtração é: ${n1 - n2}`) */
+
+// 7
+/* let n1 = 1,
+  n2 = 5
+
+alert(`O resultado da multiplicação é: ${n1 * n2}`) */
+
+// 8
+/* let n1 = 1,
+  n2 = 0.5
+
+alert(`O resultado da subtração é: ${n1 / n2}`) */
+
+// 9
+/* let n = 4
+
+n % 2 == 0 ? alert(`É um número par`) : alert('Não é um número par') */
+
+// 10
+/* let n = 4
+
+n % 2 != 0 ? alert(`É um número ímpar`) : alert('Não é um número ímpar') */
